d12aa9b151c2860a981237cb63c8d908edbbcc09
Wendy Smoak <wsmoak@gmail.com>
2015-05-18 21:51:27 -0400

    Add a new event. Switch from individual routes to resources.

--- config/routes.rb
+++ config/routes.rb
@@ -56,12 +56,14 @@ Rails.application.routes.draw do
 
   root "events#index"
 
-  get "events" => "events#index"
+  #get "events" => "events#index"
 
-  get "events/:id" => "events#show", as: "event"
+  #get "events/:id" => "events#show", as: "event"
 
-  get "events/:id/edit" => "events#edit", as: "edit_event"
+  #get "events/:id/edit" => "events#edit", as: "edit_event"
 
-  patch "events/:id" => "events#update"
+  #patch "events/:id" => "events#update"
+
+  resources :events
 
 end
